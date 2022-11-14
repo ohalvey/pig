4910c29b030dabf7fb8882e6f148a3f8d61d74a1
oliver halvey <oliverhalvey310@yahoo.com>
2022-11-14 07:37:56 -0800

    Initial Commit

---
+++ pig/pigApp.swift
@@ -0,0 +1,17 @@
+//
+//  pigApp.swift
+//  pig
+//
+//  Created by Oliver Halvey on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct pigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
